b04461ee46c3e5143cbce20fdb7c98cc8c79d611
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-11 23:23:21 +0600

    ternary

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int res = (first>second)?
+                    (first>third)?
+                            first:
+                            third>second?
+                                    third:second
+                :(second>third)?second:third;
+
+        System.out.println(res);
     }
 }
